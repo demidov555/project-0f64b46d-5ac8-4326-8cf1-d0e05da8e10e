dd4c8033c30b785558ff2af220e1b7741da36b39
demidov555 <miha.777vip@mail.ru>
2026-02-07 11:04:15 +0300

    Patch update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/project-0f64b46d-5ac8-4326-8cf1-d0e05da8e10e'
+  base: '/project-0f64b46d-5ac8-4326-8cf1-d0e05da8e10e',
 });
