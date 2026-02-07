6c9f0685aa60ab7143a08f44eb375ec7cc81131e
demidov555 <miha.777vip@mail.ru>
2026-02-07 11:02:05 +0300

    Patch update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 
 const App: React.FC = () => {
-    const [tasks, setTasks] = React.useState([]);
+  const [tasks, setTasks] = React.useState([]);
 
-    React.useEffect(() => {
-        fetch('https://project-e506628f-8ee9-434a-9890.onrender.com/tasks')
-            .then(response => response.json())
-            .then(data => setTasks(data));
-    }, []);
+  React.useEffect(() => {
+    // Placeholder for API call to fetch tasks
+  }, []);
 
-    return (
-        <div>
-            <h1>Task Management</h1>
-            <ul>
-                {tasks.map(task => (
-                    <li key={task.id}>{task.title}</li>
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Task Management</h1>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>{task.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export default App;
---
+++ frontend/vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/project-0f64b46d-5ac8-4326-8cf1-d0e05da8e10e'
+});
